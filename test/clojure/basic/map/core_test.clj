ddc9c132373c3fffcb3fc57cedad9ee55a3a4293
likanug <likanug.g@qq.com>
2025-07-02 20:01:07 +0800

    Add tests for assoc and dissoc functions in the map namespace

--- test/clojure/basic/map/core_test.clj
+++ test/clojure/basic/map/core_test.clj
@@ -9,3 +9,20 @@
     (is (= (the-map :c)  (:c the-map) 3))
     )
   )
+
+(deftest assoc-the-map-test
+  (testing "FIXME, I fail."
+    (is (= (assoc the-map :d 4) {:a 1 :b 2 :c 3 :d 4}))
+    (is (= (assoc the-map :a 10) {:a 10 :b 2 :c 3}))
+    (is (= (assoc the-map :e nil) {:a 1 :b 2 :c 3 :e nil}))
+    )
+  )
+
+(deftest dissoc-the-map-test
+  (testing "FIXME, I fail."
+    (is (= (dissoc the-map :a) {:b 2 :c 3}))
+    (is (= (dissoc the-map :b) {:a 1 :c 3}))
+    (is (= (dissoc the-map :c) {:a 1 :b 2}))
+    (is (= (dissoc the-map :d) {:a 1 :b 2 :c 3})) ; key not present
+    )
+  )
