e9f23c98e5f533ed2a8a673f35f5851c4f6dacad
likanug <likanug.g@qq.com>
2025-07-02 19:24:46 +0800

    Add core functionality and initial tests for the map namespace

---
+++ src/clojure/basic/map/core.clj
@@ -0,0 +1,3 @@
+(ns clojure.basic.map.core)
+
+(def the-map {:a 1 :b 2 :c 3})
---
+++ test/clojure/basic/map/core_test.clj
@@ -0,0 +1,11 @@
+(ns clojure.basic.map.core_test
+  (:require [clojure.test :refer :all]
+            [clojure.basic.map.core :refer :all]))
+
+(deftest the-map-test
+  (testing "FIXME, I fail."
+    (is (= (the-map :a)  (:a the-map) 1))
+    (is (= (the-map :b)  (:b the-map) 2))
+    (is (= (the-map :c)  (:c the-map) 3))
+    )
+  )
