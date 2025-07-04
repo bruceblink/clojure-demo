8ab8fda86914b4ff5b2c905ed84554c7d649a10b
likanug <likanug.g@qq.com>
2025-07-04 22:20:24 +0800

    添加doseq的示例代码

--- src/clojure/basic/loop.clj
+++ src/clojure/basic/loop.clj
@@ -9,3 +9,15 @@
       )
   )
 
+(defn run-report [user]
+  (println "Running report for user:" user))
+
+(run-report "John Doe")
+
+(defn dispatch-reporting-jobs [all-users]
+  (doseq [user all-users]
+    (run-report user)
+    )
+  )
+
+(dispatch-reporting-jobs ["Alice" "Bob" "Charlie"])
