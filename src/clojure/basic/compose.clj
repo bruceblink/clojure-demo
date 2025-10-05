83ffd43388482d84c3df87a3ceed790fd4b37469
likanug <likanug.g@qq.com>
2025-10-05 20:32:57 +0800

    add "compose" example

---
+++ src/clojure/basic/compose.clj
@@ -0,0 +1,21 @@
+(ns clojure.basic.compose)
+
+(defn negated-sum-str
+  [& numbers]
+  (str (- (apply + numbers)))
+  )
+(println (negated-sum-str 10 12 3.4) )       ;; "-25.4"
+
+(def negated-sum-str1 (comp str - +))
+(println (negated-sum-str1 10 12 3.4) )      ;; "-25.4"
+
+(require '[clojure.string :as str])
+(def caml->keyword (comp keyword
+                         str/join
+                         (partial interpose \-)
+                         (partial map str/lower-case)
+                         #(str/split % #"(?<=[a-z])(?=[A-Z])"))) ;;找到一个位置，它左边是小写字母，右边是大写字母。
+
+(str/split "CamelCase" #"(?<=[a-z])(?=[A-Z])")    ;; ["Camel" "Case"]
+
+(println (caml->keyword "CamelCase"))    ;; :camel-case
