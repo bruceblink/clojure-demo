37b7e23f3735d00059b93135f791ae2d13b840f4
likanug <likanug.g@qq.com>
2025-10-06 20:38:39 +0800

    add reduce test

---
+++ test/clojure/basic/reduce_test.clj
@@ -0,0 +1,10 @@
+(ns clojure.basic.reduce-test
+  (:require [clojure.test :refer :all])
+  )
+
+
+(deftest reduce-test
+  (testing "FIXME, I fail."
+    (is (reduce max [0 -3 10 48])  48)
+    )
+  )
