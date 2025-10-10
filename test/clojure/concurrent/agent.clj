c53374aca945d47d21296fc790183a6e686cf92f
likanug <likanug.g@qq.com>
2025-10-10 13:38:17 +0800

    update "agent" example

--- test/clojure/concurrent/agent.clj
+++ test/clojure/concurrent/agent.clj
@@ -1,13 +1,11 @@
 (ns clojure.concurrent.agent
   (:require [clojure.test :refer :all]
-            [clojure.concurrent.agent :refer :all]
             ))
 
 
 (deftest agent-test
   (testing "FIXME, I fail."
-    (is @total-cpu-time
-        0
-        )
+    (is @total-cpu-time 0 )
+    (is (send total-cpu-time + 700) 700 )
     )
   )
