38e149dd302032507910e7119057b1114b08befa
likanug <likanug.g@qq.com>
2025-10-16 22:54:31 +0800

    简化vector-test的单元测试

--- test/clojure/basic/vector_test.clj
+++ test/clojure/basic/vector_test.clj
@@ -5,17 +5,17 @@
 
 (deftest vector-test
   (testing "FIXME, I fail."
-    (is (= v (vector 1 2 3 4)))
-    (is (= "Ram" (first friends) ))
-    (is (= ["Bashir" "Antony" "Buddha"] (rest friends) ))
-    (is (= "Buddha" (friends 3) ))
-    (is (= ["Pericardia" "Ram" "Bashir" "Antony" "Buddha"] (cons "Pericardia" friends)))
-    (is (= ["Ram" "Bashir" "Antony" "Buddha"] friends))     ;; cons doesn't modify friend
+    (is v (vector 1 2 3 4))
+    (is "Ram" (first friends ))
+    (is ["Bashir" "Antony" "Buddha"] (rest friends))
+    (is "Buddha" (friends 3))
+    (is ["Pericardia" "Ram" "Bashir" "Antony" "Buddha"] (cons "Pericardia" friends))
+    (is ["Ram" "Bashir" "Antony" "Buddha"] friends)     ;; cons doesn't modify friend
     )
   )
 
 (deftest vector-type
   (testing "FIXME, I fail."
-    (is (= PersistentVector (type friends) ))
+    (is PersistentVector (type friends))
     )
   )
