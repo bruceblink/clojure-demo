26ae4fdba3939c7cde8dd9bae9c5379e9155cf1b
likanug <likanug.g@qq.com>
2025-10-17 11:44:15 +0800

    新增多参数函数定义的使用示例代码

---
+++ src/clojure/basic/function.clj
@@ -0,0 +1,42 @@
+(ns clojure.basic.function)
+(defn hypotenuse [a b]                                      ;; multiple arguments
+  (Math/sqrt (+ (* a a) (* b b)))
+  )
+
+;; 定义不定参数函数
+(defn unlimited-arguments [& args]
+  (println "Arguments:" args)
+  (println "Type of args:" (type args)))
+
+(unlimited-arguments 1)                                     ;; Arguments: (1)
+                                                            ;; Type of args: clojure.lang.ArraySeq
+(println)
+(unlimited-arguments 1 17 true "Karthik" :coder)            ;; Arguments: (1 17 true Karthik :coder)
+                                                            ;; Type of args: clojure.lang.ArraySeq
+
+(defn unlimited-arguments1 [first-arg & args]
+  (println "First argument: " first-arg)
+  (println "Other argument: " args)
+  )
+
+(unlimited-arguments1 1)                                     ;; First argument:  1
+                                                             ;; Other argument:  nil
+
+(unlimited-arguments1 1 17 true "Karthik" :coder)            ;; First argument:  1
+                                                             ;; Other argument:  (17 true Karthik :coder)
+
+(defn unlimited-arguments2 [first-arg second-arg & args]
+  (println "First argument:" first-arg)
+  (println "Second argument:" second-arg)
+  (println "Other arguments:" args))
+
+;; (unlimited-arguments2 1)   ;; 编译报错 Incorrect arity 1 for clojure.basic.function/unlimited-arguments2
+
+(unlimited-arguments2 1 2)   ;; First argument: 1
+                            ;; Second argument: 2
+
+
+(unlimited-arguments2 1 17 true "Karthik" :coder)   ;; First argument: 1
+                                                    ;; Second argument: 17
+                                                    ;; Other arguments: (true Karthik :coder)
+
---
+++ test/clojure/basic/function_test.clj
@@ -0,0 +1,10 @@
+(ns clojure.basic.function_test
+  (:require [clojure.test :refer :all]
+            [clojure.basic.function :refer :all])
+  )
+
+(deftest test-hypotenuse
+  (testing "FIXME, I fail."
+    (is 5 (hypotenuse 3 4))
+    )
+  )
